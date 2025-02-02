efcc8a0dadb662342a754a917e40c7e2a752624f
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-02-02 19:57:15 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It Will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("ok, deletign"),
+          style: "destructive",
+        },
+        { text: "Cancel", style: "cancel" },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.items}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          style={styles.button}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
   },
+  items: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+  itemText: { fontSize: 18, fontWeight: 200 },
 });
