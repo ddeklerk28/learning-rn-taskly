0bf7659d11f15639655f4e17c6716153164a4294
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-02-02 20:37:41 +0200

    Component (new) - ShoppingListItem.tsx

--- App.tsx
+++ App.tsx
@@ -1,36 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { theme } from "./theme";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert(
-      "Are you sure you want to delete this?",
-      "It Will be gone for good",
-      [
-        {
-          text: "Yes",
-          onPress: () => console.log("ok, deletign"),
-          style: "destructive",
-        },
-        { text: "Cancel", style: "cancel" },
-      ],
-    );
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.items}>
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-          onPress={handleDelete}
-          style={styles.button}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
+      <ShoppingListItem />
     </View>
   );
 }
@@ -41,25 +16,4 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
   },
-  items: {
-    borderBottomWidth: 1,
-    borderBottomColor: theme.colorCerulean,
-    paddingHorizontal: 8,
-    paddingVertical: 16,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: 1,
-  },
-  itemText: { fontSize: 18, fontWeight: 200 },
 });
---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,56 @@
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "../theme";
+
+export const ShoppingListItem = () => {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It Will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("ok, deletign"),
+          style: "destructive",
+        },
+        { text: "Cancel", style: "cancel" },
+      ],
+    );
+  };
+
+  return (
+    <View style={styles.item}>
+      <Text style={styles.itemText}>Coffee</Text>
+      <TouchableOpacity
+        onPress={handleDelete}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+  itemText: { fontSize: 18, fontWeight: 200 },
+});
