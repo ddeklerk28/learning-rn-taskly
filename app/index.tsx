ef3509c65668d0bcce80038d33139c7c14665159
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-02-18 22:08:46 +0200

    Refactor: Replaced ScrollView with more performant FlatList

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,62 @@
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, TextInput, View, Text } from 'react-native';
 import { theme } from '../theme';
 import { ShoppingListItem } from '../components/ShoppingListItem';
+import { useState } from 'react';
+
+type TShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: TShoppingListItemType[] = [
+  { id: '1', name: 'Coffee' },
+  { id: '2', name: 'Tea' },
+  { id: '3', name: 'Sugar' },
+];
 
 export default function Index() {
+  const [shoppingList, setShoppingList] =
+    useState<TShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState('');
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList: TShoppingListItemType[] = [
+        { id: new Date().toTimeString(), name: value },
+        ...shoppingList,
+      ];
+
+      setShoppingList(newShoppingList);
+      setValue('');
+    }
+  };
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted={true} />
-      <ShoppingListItem name="Sugar" />
-    </View>
+    <FlatList
+      data={shoppingList}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>You shopping list is empty</Text>
+        </View>
+      }
+      ListHeaderComponent={
+        <TextInput
+          placeholder="E.g. Coffee"
+          style={styles.textInput}
+          value={value}
+          onChangeText={setValue}
+          keyboardType="default"
+          returnKeyType="done"
+          onSubmitEditing={handleSubmit}
+        />
+      }
+      renderItem={({ item }) => {
+        return <ShoppingListItem name={item.name} />;
+      }}
+    />
   );
 }
 
@@ -16,6 +64,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    backgroundColor: theme.colorWhite,
+  },
+  listEmptyContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 18,
   },
 });
