3bdcf905a0af34b2c9651acff231e7d64c189f9a
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-03-02 18:08:27 +0200

    Feat: added ability to request push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { theme } from '../../theme';
+import { registerForPushNotificationsAsync } from '../../utils/push-notifications';
 
 export default function CounterScreen() {
+  const handleRequestPermission = async () => {
+    const result = await registerForPushNotificationsAsync();
+
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={handleRequestPermission}
+      >
+        <Text style={styles.buttonText}>Request Permission</Text>
+      </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
     </View>
   );
@@ -15,6 +29,17 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colorWhite,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
   },
   text: {
     fontSize: 24,
