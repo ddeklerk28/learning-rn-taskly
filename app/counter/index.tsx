18c76b305610a7aba2284c382d4f8b307ad0d8d4
Dirk de Klerk <dirk.deklerk@blackswan.com>
2025-03-14 17:42:11 +0200

    Added placeholder counter

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -5,6 +5,7 @@ import { PermissionStatus } from 'expo-modules-core';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { SchedulableTriggerInputTypes } from 'expo-notifications';
+import { useEffect, useState } from 'react';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -15,6 +16,18 @@ Notifications.setNotificationHandler({
 });
 
 export default function CounterScreen() {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsElapsed((prevSecond) => Number(prevSecond + 1));
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
 
@@ -47,7 +60,7 @@ export default function CounterScreen() {
       >
         <Text style={styles.buttonText}>Schedule Notification</Text>
       </TouchableOpacity>
-      <Text style={styles.text}>Counter</Text>
+      <Text style={styles.text}>{secondsElapsed}</Text>
     </View>
   );
 }
